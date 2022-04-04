55fbb8b0803429d3bf4f83a3ef6693578d6c87cf
tesla <denizvesky@gmail.com>
2022-04-04 19:33:43 +0200

    absolute_value and substring added

---
+++ absolute_value.py
@@ -0,0 +1,10 @@
+i#define a function named absolute_value to calculate and return absolute value of the entered number . you can add docstring for an explanation
+
+def absolute_value(num):
+    """This function returns to absolute value of the given number"""
+    if num >= 0 :
+        return num
+    else :
+        return -num    
+
+
